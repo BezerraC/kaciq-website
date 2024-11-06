af301863fbb5e2428f8c87d512ede8510a2c5d96
BezerraC <cbezerraneto@gmail.com>
2024-11-06 14:26:17 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Configuração do EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Diretório de arquivos públicos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Importando rotas
+const indexRoutes = require('./routes/index');
+const contactRoutes = require('./routes/contact');
+const aboutRoutes = require('./routes/about');
+
+// Usando rotas
+app.use('/', indexRoutes);
+app.use('/about', aboutRoutes);
+app.use('/contact', contactRoutes);
+
+app.use((req, res) => {
+    res.status(404).render("layout", {
+        body: '404',
+        title: "Error 404",
+      });;
+});
+
+
+// Iniciando o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
